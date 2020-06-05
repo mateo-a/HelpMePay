e71005274260d8b5e14f7ee569881d74fa2977f8
andres <andressantaigore@gmail.com>
2020-06-04 23:26:39 -0500

    [Feature] Formato mejorado para vistas login, empty_state y terms_service

--- lib/terms_service.dart
+++ lib/terms_service.dart
@@ -4,31 +4,40 @@ class TermsService extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Terminos y Condiciones'),
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            child: Text('Lorem ipsum dolor sit amet, '
-                'consectetur adipiscing elit, sed do '
-                'eiusmod tempor incididunt ut labore '
-                'et dolore magna aliqua. Ut enim ad '
-                'minim veniam, quis nostrud exercitation'
-                ' ullamco laboris nisi ut aliquip ex ea'
-                'commodo consequat. Duis aute irure'
-                'dolor in reprehenderit in voluptate'
-                'velit esse cillum dolore eu fugiat'
-                'nulla pariatur. Excepteur sint'
-                'occaecat cupidatat non proident, '
-                'sunt in culpa qui officia deserunt '
-                'mollit anim id est laborum.'),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(14),
+              child: Text('Lorem ipsum dolor sit amet, '
+                  'consectetur adipiscing elit, sed do '
+                  'eiusmod tempor incididunt ut labore '
+                  'et dolore magna aliqua. Ut enim ad '
+                  'minim veniam, quis nostrud exercitation '
+                  ' ullamco laboris nisi ut aliquip ex ea '
+                  'commodo consequat. Duis aute irure '
+                  'dolor in reprehenderit in voluptate '
+                  'velit esse cillum dolore eu fugiat '
+                  'nulla pariatur. Excepteur sint '
+                  'occaecat cupidatat non proident, '
+                  'sunt in culpa qui officia deserunt '
+                  'mollit anim id est laborum.'),
+            ),
           ),
-          Container (
+          Container(
+            padding: EdgeInsets.all(14),
             child: Text('Al proceder aceptas nuestros '
                 'terminos de servicio y nuestra política '
                 'de privacidad'),
           ),
           Container(
+            padding: EdgeInsets.all(14),
             child: RaisedButton(
               onPressed: () {},
               child: const Text('Acepto condiciones'),
