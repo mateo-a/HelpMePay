230021f49053c0bec216eb5c3584361759a438a2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-06-24 17:01:00 -0500

    update: refactor code menyslider widget and dream detail page

---
+++ lib/widgets/dreamdetail.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../models/loan.dart';
+import 'moneyslider.dart';
+
+class DetailPage extends StatelessWidget {
+//Detalle de sueño/historia
+  final Loan loan;
+
+  DetailPage(this.loan);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loan.titulo),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+            child: CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage("NP"),
+            ),
+          ),
+          Container(
+            child: Wrap(
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      loan.descripcion,
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+            width: 150.0,
+            child: Divider(
+              color: Colors.blue,
+            ),
+          ),
+          Container(
+            child: ButtonBar(
+              alignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Expira pronto'),
+                  onPressed: (){},
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MoneySlider()));
+                  },
+                  child: Text('Ayudalo!'),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
