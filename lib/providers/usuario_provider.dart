c7660806b224077817c7b205bfed744e70e90e10
mateo-a <alexander.urrego@gmail.com>
2020-07-03 21:46:03 -0500

    [Feature] Adding features to authenticate with firebase

---
+++ lib/providers/usuario_provider.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+// import 'package:flutterapp/preferencias_usuario/preferencias_usuario.dart';
+import 'package:http/http.dart' as http;
+
+class UsuarioProvider {
+
+  final String _firebaseToken = 'AIzaSyDqtd-gB1SIiMLbsKx3NqnHWV2khW5GrYA';
+  //final _prefs = new PreferenciasUsuario();
+
+  Future<Map<String, dynamic>> login( String email, String password) async {
+    final authData = {
+      'email'             : email,
+      'password'          : password,
+      'returnSecureToken' : true
+    };
+    final resp = await http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken',
+      body: json.encode( authData )
+    );
+    Map<String, dynamic> decodedResp = json.decode( resp. body );
+    if ( decodedResp.containsKey('idToken')){
+      
+        // _prefs.token    = decodedResp['idToken'];
+        // _prefs.localid  = decodedResp['localId'];
+
+      return {'ok': true, 'token': decodedResp['idToken']};
+    } else {
+      return {'ok': false, 'message': decodedResp['error']['message']};
+    }
+  } 
+
+
+
+  Future<Map<String, dynamic>> nuevoUsuario( String email, String password) async {
+    final authData = {
+      'email'             : email,
+      'password'          : password,
+      'returnSecureToken' : true
+    };
+    final resp = await http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$_firebaseToken',
+      body: json.encode( authData )
+    );
+    Map<String, dynamic> decodedResp = json.decode( resp. body );  
+    if ( decodedResp.containsKey('idToken')){
+
+        // _prefs.token    = decodedResp['idToken'];
+        // _prefs.localid  = decodedResp['localId'];
+      return {'ok': true, 'localId': decodedResp['localId']};
+    } else {
+      return {'ok': false, 'message': decodedResp['error']['message']};
+    }
+  }
+}
