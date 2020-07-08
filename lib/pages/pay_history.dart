388c400d27d3a16bfc38648fa7b1eac6fb3bc105
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-08 12:47:04 -0500

    update: changes in how to pay each installment

--- lib/pages/pay_history.dart
+++ lib/pages/pay_history.dart
@@ -1,6 +1,8 @@
 //import 'package:banking_app_account_ui/app_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/blocs/provider.dart';
+import 'package:flutterapp/models/cuotasnegocio_model.dart';
+//import 'package:flutterapp/models/loan_model.dart';
 import 'package:flutterapp/models/pago_model.dart';
 import 'package:flutterapp/pages/drawer_b.dart';
 
@@ -53,41 +55,14 @@ class _PayHistoryState extends State<PayHistory> {
       child: Scaffold(
         drawer: MenuDrawerB(),
         appBar: AppBar(
-          title: Text("Historial de pagos"),
+          title: Text("Lista de Cuotas"),
           iconTheme: IconThemeData(
             color: Colors.white, //change your color here
           ),
           backgroundColor: Colors.blue[700],
           elevation: 0.0,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.blue,
-              ),
-              onPressed: () {},
-            )
-          ],
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Column(
-              children: <Widget>[
-                topArea(),
-                SizedBox(
-                  height: 40.0,
-                  child: Icon(
-                    Icons.refresh,
-                    size: 35.0,
-                    color: Colors.blue[700],
-                  ),
-                ),
-                displayAccoutList(),
-              ],
-            ),
-          ),
-        ),
+        body: _displayAccoutList(loanBloc),
         bottomNavigationBar: BottomAppBar(
           elevation: 0.0,
           child: Container(
@@ -102,8 +77,9 @@ class _PayHistoryState extends State<PayHistory> {
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20.0)),
                   color: Colors.green[500],
-                  //borderSide: BorderSide(color: Color(0xFF015FFF), width: 1.0),
-                  onPressed: () {_realizarPago(context, loanBloc);},
+                  onPressed: () {
+                    _realizarPago(context, loanBloc);
+                  },
                   child: Text("Realizar un Pago",
                       style: TextStyle(color: Colors.white)),
                 ),
@@ -118,58 +94,45 @@ class _PayHistoryState extends State<PayHistory> {
   _realizarPago(BuildContext context, LoanBloc loanBloc) {
     final PagoModel pago = new PagoModel();
 
-    pago.idcuota   = "cuota2";                    //ID Cuota
-    pago.idnegocio = "woOlgcKsxaxLsJf52WXX";      // ID Negocio
+    pago.idcuota = "cuota2"; //ID Cuota
+    pago.idnegocio = "woOlgcKsxaxLsJf52WXX"; // ID Negocio
 
     loanBloc.realizarPago(pago);
   }
 
-  Container accountItems(
-          String item, String charge, String dateString, String type,
-          {Color oddColour = Colors.white}) =>
-      Container(
-        decoration: BoxDecoration(color: oddColour),
-        padding:
-            EdgeInsets.only(top: 20.0, bottom: 20.0, left: 5.0, right: 5.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(item, style: TextStyle(fontSize: 16.0)),
-                Text(charge, style: TextStyle(fontSize: 16.0))
-              ],
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(dateString,
-                    style: TextStyle(color: Colors.grey, fontSize: 14.0)),
-                Text(type, style: TextStyle(color: Colors.grey, fontSize: 14.0))
-              ],
-            ),
-          ],
-        ),
-      );
+  Widget _displayAccoutList(LoanBloc loanBloc) {
+    loanBloc.cuotasNegocio("ENWTpce6OxCFQRgy1rMu");
+    return StreamBuilder(
+        stream: loanBloc.cuotasNegocioStream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            print(snapshot);
+            final cuotas = snapshot.data;
+            return ListView.builder(
+              itemCount: cuotas.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  _crearItem(context, loanBloc, cuotas[index]),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
+  }
 
-  displayAccoutList() {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      child: Column(
-        children: <Widget>[
-          accountItems("Transferencia", r"+ $ 40.000", "24-05-21", "",
-              oddColour: const Color(0xFFF7F7F9)),
-          accountItems("Transferencia", r"+ $ 550.000", "28-06-21", ""),
-          accountItems("Consignación", r"+ $ 50.000", "25-07-21", "",
-              oddColour: const Color(0xFFF7F7F9)),
-          accountItems("Transferencia", r"+ $ 200.000", "28-08-21", ""),
-          accountItems("Transferencia", r"+ $ 150.000", "16-09-21", "",
-              oddColour: const Color(0xFFF7F7F9)),
-        ],
-      ),
+  Widget _crearItem(
+      BuildContext context, LoanBloc loanBloc, CuotasNegocioModel cuotas) {
+    var raisedButton = RaisedButton(
+        color: Colors.green[500],
+        onPressed: _realizarPago(context, loanBloc),
+        child: Text("Pagar", style: TextStyle(color: Colors.white)));
+    return ListTile(
+      title: Text('id de cuota: ${cuotas.id}'),
+      contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+      subtitle: Text(
+          'total a pagar: ${cuotas.data.aporte.toString()}\nfecha de pago: ${cuotas.data.fechapago}'),
+      trailing: raisedButton,
     );
   }
 }
